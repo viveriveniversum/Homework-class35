ceae93e22a0843feed4cf08335060cce9f701e79
Onur Manap <74452204+viveriveniversum@users.noreply.github.com>
2022-01-21 00:15:08 +0100

    Ex4 is finished and tested

--- 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
+++ 2-Browsers/Week1/homework/ex4-whatsTheTime/index.js
@@ -8,7 +8,37 @@ Full description at: https://github.com/HackYourFuture/Homework/tree/main/2-Brow
 2. Have the function execute when it's loading in the browser.
 ------------------------------------------------------------------------------*/
 function addCurrentTime() {
-  // TODO complete this function
+
+  const body = document.querySelector('body');
+  body.style.height = '%100';
+  body.style.display = 'flex';
+  body.style.flexDirection = 'column';
+  body.style.minHeight = '100vh'
+  body.style.alignItems = 'center';
+  body.style.justifyContent = 'center';
+  body.style.background = 'linear-gradient(to right,#4e54c8,#8f94fb)';
+  const div = document.createElement('div');
+  div.style.background = '#fffc00';
+  div.style.color = '#000';
+  div.style.minWidth = '20vw';
+  div.style.height = '12vh'
+  div.style.textAlign = 'center'
+  div.style.fontSize = '4rem';
+  div.style.padding = '1rem'
+  div.style.boxShadow = '10px 10px 6px #fdf133';
+
+
+  
+  body.appendChild(div)
+  const timer = () =>{
+    const date = new Date()
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    div.textContent = `${hours} : ${minutes} : ${seconds}`;
+  }
+  setInterval(timer,1000);
+
 }
 
-// TODO execute `addCurrentTime` when the browser has completed loading the page
+window.addEventListener('load',addCurrentTime);
